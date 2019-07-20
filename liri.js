9ad4715c5d72cb0d8f02d3f112443b73908358f5
Meng Hao <jaysumampong@gmail.com>
2019-07-20 17:51:17 -0600

    added baseline function

--- liri.js
+++ liri.js
@@ -1,1 +1,31 @@
-require("dotenv").config();
+let dotEnv = require("dotenv").config();
+let keys = require("./keys.js");
+let spotify = new Spotify(keys.spotify);
+let axios = require("axios");
+let fs = require("fs");
+let moment = require("moment");
+
+let search = process.argv[2];
+let term = process.arvg[3];
+
+// command line key words
+switch {
+    case
+}
+
+// bands function
+let bands = function() {
+
+}    
+
+
+// spotify function
+let SpotifyThisSong = function() {
+
+}    
+
+// movie function
+let movie = function () {
+
+}    
+
