1e10b07951172b025a1fd7879734a33779f55e4d
Meng Hao <jaysumampong@gmail.com>
2019-07-20 18:09:47 -0600

    wronte spotify function

--- liri.js
+++ liri.js
@@ -9,23 +9,60 @@ let search = process.argv[2];
 let term = process.arvg[3];
 
 // command line key words
-switch {
-    case
+switch (search) {
+    case ("concert-this"):
+        concertThis();
+        break;
+    case ("spotify-this-song"):
+        if (search) {
+            SpotifyThisSong(search);
+        } 
+        else {
+            SpotifyThisSong("The Sign");
+        }
+    
 }
 
 // bands function
-let bands = function() {
+let concertThis = function() {
 
 }    
 
 
 // spotify function
 let SpotifyThisSong = function() {
+    spotify.search(
+        {
+            type: "track",
+            name: songName
+        },
+        function(error, data) {
+            if (error){
+                console.log("Error" + error);
+                return;
+            }
+            let spotifySongs = data.tracks.items;
 
-}    
+            for (let i = 0; i < spotifySongs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+
+            }
+
+        }
+    );
+};
 
 // movie function
-let movie = function () {
+let movieThis = function() {
 
 }    
+
+let doWhatitSays = function() {
+
+}
 
