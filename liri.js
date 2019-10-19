5372982f4de312ca69f53e2b0db4694d6635b5c9
Jay Sumampong <jaysumampong@gmail.com>
2019-10-18 23:44:09 -0600

    scrapped current liri.js

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 let keys = require("./keys.js");
-// let spotify = new Spotify(keys.spotify);
-// let Spotify = require("node-spotify-api");
+let spotify = new Spotify(keys.spotify);
+let Spotify = require('node-spotify-api');
 let axios = require("axios");
 let fs = require("fs");
 let moment = require("moment");
@@ -15,22 +15,3 @@ for (let i = 3; i < process.argv.length; i++) {
     input += element + " ";
 }
 
-switch (operators) {
-    case "concert-this":
-        if (!input) {
-            input = "queen"
-        }
-        query = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
-        axios.get(query).then(function(data) {
-            for (let i = 0; i < data.data.length; i++) {
-                console.log(data.data[i].venue.name + ' ' + data.data[i].venue.city + ', ' + 
-                data.data[i].venue.country + ' ' + 
-                moment(data.data[i].datetime).format('MMMM Do YYYY, h:mm a'))
-            }
-        })
-        break;
-    case "spotify-this":
-        break;
-    case "movie-this":
-        break;
-}
