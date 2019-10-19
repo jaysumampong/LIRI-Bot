8b236d1a4e2bbac83ed63076f74304af6cad9a17
Jay Sumampong <jaysumampong@gmail.com>
2019-10-19 00:04:57 -0600

    updated

--- liri.js
+++ liri.js
@@ -1,17 +1,12 @@
 require("dotenv").config();
 
 let keys = require("./keys.js");
-let spotify = new Spotify(keys.spotify);
-let Spotify = require('node-spotify-api');
+// let spotify = new Spotify(keys.spotify);
+// let Spotify = require("node-spotify-api");
 let axios = require("axios");
 let fs = require("fs");
 let moment = require("moment");
 
 let operators = process.argv[2];
 let input = "";
-
-for (let i = 3; i < process.argv.length; i++) {
-    let element = process.argv[i];
-    input += element + " ";
-}
 
